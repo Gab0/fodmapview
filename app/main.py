9998a76b2d63a0864bae282f2f3f87978b29bb03
Gabriel Araujo <gabriel_scf@hotmail.com>
2019-02-20 15:27:41 -0300

    .

--- app/main.py
+++ app/main.py
@@ -6,10 +6,97 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 
+from kivy.uix.widget import Widget
+from kivy.graphics import Ellipse, Color
+
 from kivy.uix.image import Image
 from database import DatabaseManager
-
 import json
+
+import ssl
+
+# this makes https work on android;
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+class Sphere(Widget):
+    def __init__(self, layout):
+        Widget.__init__(self)
+        layout.bind(pos=self.update, size=self.update)
+
+        self.w = Ellipse(pos=self.pos, size=self.size)
+
+        self.Color = Color(rgb=(1,1,1))
+        self.canvas.add(self.Color)
+        self.canvas.add(self.w)
+
+        self.bind(pos=self.update, size=self.update)
+
+    def update(self, instance, value):
+        self.w.pos = instance.pos
+        self.w.size = instance.size
+        print(value)
+
+    def changecolor(self, color):
+        self.Color.rgb = color
+
+
+class AttributeViewer(GridLayout):
+    def __init__(self):
+        super(AttributeViewer, self).__init__()
+        self.Name = Label()
+        self.Category = Label(text="")
+
+        self.cols = 1
+        self.rows = 6
+
+        Spheres = GridLayout()
+        Spheres.rows = 1
+        Spheres.cols = 4
+        self.coloredSpheres = {
+            "oligos": Sphere(Spheres),
+            "fructose": Sphere(Spheres),
+            "polyols": Sphere(Spheres),
+            "lactose": Sphere(Spheres)
+        }
+        Spheres.add_widget(self.coloredSpheres['oligos'])
+        Spheres.add_widget(self.coloredSpheres['fructose'])
+        Spheres.add_widget(self.coloredSpheres['polyols'])
+        Spheres.add_widget(self.coloredSpheres['lactose'])
+
+        subtitles = Label(text="oligos/fructose/polyols/lactose")
+
+        self.add_widget(self.Name)
+        self.add_widget(self.Category)
+        self.add_widget(Label())
+        self.add_widget(Label(text="Content:"))
+        self.add_widget(subtitles)
+        self.add_widget(Spheres)
+
+    def changeView(self, data):
+        self.Name.text = data["name"]
+        self.Category.text = data["category"]
+
+        Values = {
+            "low": 10,
+            "high": 90
+        }
+
+        cm = 0.2
+        cM = 0.8
+
+        ballColorMap = {
+            0: (cm, cM, cm), # GREEN
+            1: (cm, cM, cM), # YELLOW
+            2: (cM, cm, cm)  # RED
+        }
+        for k in self.coloredSpheres.keys():
+            try:
+                self.coloredSpheres[k].changecolor(ballColorMap[data["details"][k]])
+            except KeyError as e:
+                print('fail')
+            except Exception as e:
+                raise
 
 
 class Viewer(GridLayout):
@@ -27,9 +114,12 @@ class Viewer(GridLayout):
         self.random_button = Button(text="RANDOM", on_press=lambda a: self.randomEntry())
         self.Image = Image()
 
+        self.foodView = AttributeViewer()
+
         # ADDING WIDGETS
         self.add_widget(self.Image)
-        self.add_widget(self.showText)
+        #self.add_widget(self.showText)
+        self.add_widget(self.foodView)
         self.add_widget(self.random_button)
 
     def randomEntry(self):
@@ -47,6 +137,8 @@ class Viewer(GridLayout):
             print("Failure to connect")
             print(str(e))
         self.showText.text = json.dumps(data, indent=2)
+
+        self.foodView.changeView(data)
 
 
 class fodmapApp(App):
